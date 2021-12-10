11bab555cdd5e662a3d3d932e2f4a1acfff483db
bliaowey <62267175+bliaowey@users.noreply.github.com>
2021-12-10 15:00:26 -0500

    Update Mongolia.js

--- components/Mongolia.js
+++ components/Mongolia.js
@@ -14,8 +14,6 @@ export default function App(){
             Mongolian National Anthem: "Mongol Ulsiin töriin duulal"
             </Text>
           </View>
-          <View style={styles.horizontal}>
-          </View>
           <View style={styles.container}>
             <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
             <Text style={{fontSize: 20}}>
