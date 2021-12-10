32b4b086d721c273ede4fee706e4442abc5e144e
bliaowey <62267175+bliaowey@users.noreply.github.com>
2021-12-10 14:58:21 -0500

    Update Canada.js

--- components/Canada.js
+++ components/Canada.js
@@ -11,8 +11,6 @@ export default function App(){
             Canadian National Anthem: "O Canada"
             </Text>
           </View>
-          <View style={styles.horizontal}>
-          </View>
           <View style={styles.container}>
             <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
             <Text style={{fontSize: 20, padding: 10}}>
