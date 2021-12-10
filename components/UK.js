6c606913a6cfca21f63d33ac3e4cd23ef96c9294
bliaowey <62267175+bliaowey@users.noreply.github.com>
2021-12-10 15:02:28 -0500

    Update UK.js

--- components/UK.js
+++ components/UK.js
@@ -11,8 +11,6 @@ export default function App(){
             United Kingdom National Anthem: "God Save the Queen"
             </Text>
           </View>
-          <View style={styles.horizontal}>
-          </View>
           <View style={styles.container}>
             <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
             <Text style={{fontSize: 20, padding: 10}}>
