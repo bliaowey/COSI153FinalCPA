bb49256007cccfb4bbdfcbb913bace602200ec1d
bliaowey <62267175+bliaowey@users.noreply.github.com>
2021-12-10 12:54:44 -0500

    Update USA.js

--- components/USA.js
+++ components/USA.js
@@ -12,10 +12,6 @@ export default function App(){
             </Text>
           </View>
           <View style={styles.horizontal}>
-            <Image
-              style={{width: 200, height: 100}}
-              source={{uri: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png'}}
-            />
           </View>
           <View style={styles.container}>
             <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
@@ -37,6 +33,7 @@ export default function App(){
                 O say, does that star-spangled banner yet wave {"\n"}
                 O'er the land of the free and the home of the brave
             </Text>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/w5x_9KEogM8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </View>
           </View>
         </View>
